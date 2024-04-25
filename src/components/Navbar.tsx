6573b96c62c798535a985dbc54deef930e73e6d9
efmorales <54375116+efmorales@users.noreply.github.com>
2024-04-25 10:01:16 -0400

    added styling animations fornav buttons in desktop and mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,21 +11,21 @@ function Navbar() {
       <div className="bg-emerald-100 w-32 h-full flex justify-center items-center">
         <HomeIcon className="h-6 w-6 text-black" />
       </div>
-      <div className="hidden sm:flex px-6 gap-4">
-        <button className="text-white">About</button>
-        <button className="text-white">Projects</button>
-        <button className="text-white">Contact</button>
+      <div className="hidden sm:flex px-6 h-full gap-4">
+        <button className="text-white hover:bg-emerald-700 px-3 py-2 rounded-md transition-colors duration-300">About</button>
+        <button className="text-white hover:bg-emerald-700 px-3 py-2 rounded-md transition-colors duration-300">Projects</button>
+        <button className="text-white hover:bg-emerald-700 px-3 py-2 rounded-md transition-colors duration-300">Contact</button>
       </div>
-      <div className="sm:hidden px-4 flex-col">
+      <div className="sm:hidden px-4 flex-col hover:bg-emerald-700 rounded-md transition-colors duration-300">
         <button
           onClick={() => setIsOpen(!isOpen)}>
           <Bars3Icon className="h-14 w-6 text-white" />
         </button>
         {isOpen && (
           <div className="absolute left-0 bg-emerald-700 w-full flex flex-col items-center py-2">
-            <button className="text-white py-2">About</button>
-            <button className="text-white py-2">Projects</button>
-            <button className="text-white py-2">Contact</button>
+            <button className="text-white hover:bg-emerald-500 px-28 py-2 rounded-md transition-colors duration-300">About</button>
+            <button className="text-white hover:bg-emerald-500 px-28 py-2 rounded-md transition-colors duration-300">Projects</button>
+            <button className="text-white hover:bg-emerald-500 px-28 py-2 rounded-md transition-colors duration-300">Contact</button>
           </div>
         )}
       </div>
