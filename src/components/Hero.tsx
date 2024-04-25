6528c263abdb424d7f8039fc6c422abc477b2839
efmorales <54375116+efmorales@users.noreply.github.com>
2024-04-25 18:28:35 -0400

    updated Hero component with responsive design and improved stylization, z-index improvements

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,26 @@
 function Hero() {
     return (
-      <div>Hero</div>
+      <div className="flex flex-col sm:flex-row justify-between w-full h-auto bg-[#00343C] py-12">
+
+        <div className="mb-4 basis-8/12 h-auto flex justify-center sm:mb-0">
+          <img className="relative z-30 w-52 h-52 rounded-full -right-12 sm:-right-12 sm:w-72 sm:h-72 sm:ml-20" 
+          src="src/assets/webProfile.png" 
+          alt="Profile Pic" />
+          <img className="relative z-50 w-16 h-16 rounded-full -bottom-36 sm:w-32 sm:h-32 sm:-left-16 sm:-bottom-36" 
+          src="src/assets/salmonEllipse.svg"
+          alt="Salmon Ellipse" />
+          <img className="relative z-40 w-24 h-24 rounded-full -left-12 -bottom-16 sm:w-40 sm:h-40 sm:-left-28 sm:-bottom-14" 
+          src="src/assets/tealEllipse.svg" 
+          alt="Teal Ellipse" />
+        </div>
+
+        <div className="flex-col basis-4/12 mx-8 content-center">
+          <h1 className="text-white text-3xl mb-2">Hey, nice to meet you!</h1>
+          <p className="font-light text-slate-300">I'm Enzo Morales, welcome to the vibrant digital gateway showcasing the innovative world of a passionate Junior Full-Stack MERN Developer. The hero section of this personal portfolio is a striking symphony of functionality and aesthetic appeal, designed to captivate and inform from the first glance.</p>
+          
+        </div>
+
+      </div>
     )
   }
   
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,22 +7,22 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="w-full h-16 bg-emerald-800 flex justify-between items-center">
-      <div className="bg-emerald-100 w-32 h-full flex justify-center items-center">
+    <div className="w-full h-16 bg-[#002F38] flex justify-between items-center">
+      <div className="bg-[#00948D] w-32 h-full flex justify-center items-center">
         <HomeIcon className="h-6 w-6 text-black" />
       </div>
       <div className="hidden sm:flex px-6 h-full gap-4">
-        <button className="text-white hover:bg-emerald-700 px-3 py-2 rounded-md transition-colors duration-300">About</button>
-        <button className="text-white hover:bg-emerald-700 px-3 py-2 rounded-md transition-colors duration-300">Projects</button>
-        <button className="text-white hover:bg-emerald-700 px-3 py-2 rounded-md transition-colors duration-300">Contact</button>
+        <button className="text-white hover:bg-[#00948D] px-3 py-2 rounded-md transition-colors duration-300">About</button>
+        <button className="text-white hover:bg-[#00948D] px-3 py-2 rounded-md transition-colors duration-300">Projects</button>
+        <button className="text-white hover:bg-[#00948D] px-3 py-2 rounded-md transition-colors duration-300">Contact</button>
       </div>
-      <div className="sm:hidden px-4 flex-col hover:bg-emerald-700 rounded-md transition-colors duration-300">
+      <div className="sm:hidden px-4 flex-col hover:bg-[#00948D] rounded-md transition-colors duration-300">
         <button
           onClick={() => setIsOpen(!isOpen)}>
           <Bars3Icon className="h-14 w-6 text-white" />
         </button>
         {isOpen && (
-          <div className="absolute left-0 bg-emerald-700 w-full flex flex-col items-center py-2">
+          <div className="absolute left-0 z-50 bg-[#00948D] w-full flex flex-col items-center py-2">
             <button className="text-white hover:bg-emerald-500 px-28 py-2 rounded-md transition-colors duration-300">About</button>
             <button className="text-white hover:bg-emerald-500 px-28 py-2 rounded-md transition-colors duration-300">Projects</button>
             <button className="text-white hover:bg-emerald-500 px-28 py-2 rounded-md transition-colors duration-300">Contact</button>
