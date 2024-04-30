52e97a2bbe3b735947c80d360f3bcd1775dbca27
efmorales <54375116+efmorales@users.noreply.github.com>
2024-04-30 14:42:19 -0400

    updated Project component and added icon

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -6,7 +6,7 @@ import EvenProjectCard from "../components/EvenProjectCard";
 
 const firstProject : {title:string; description: string; url: string; src: string;iconSrcArray: string[]; iconAltArray: string[]} = {
   title: "Philosofinder AI",
-  description : "Philosofinder’s is a FullStack MERN Web App, hosted in AWS. It’s objective is to help find it’s users the philosophies their worldview are already aligned with. Powered by OpenAI’s GPT 4.0.",
+  description : "Philosofinder is a FullStack MERN Web App, hosted in AWS. Its objective is to help find its users the diverse philosophies they might have a deep connection with the help of an AI assisted quiz. Powered by OpenAI’s GPT 4.0.",
   url: "https://philosophy.enzomorales.net",
   src: "/src/assets/pfWindow.png",
   iconSrcArray: ["/src/assets/reactIcon.svg", "/src/assets/nodeIcon.svg", "/src/assets/awsIcon.svg", "/src/assets/mdbIcon.svg"],
@@ -18,8 +18,8 @@ const secondProject : {title:string; description: string; url: string; src: stri
   description : "This project is a React TypeScript Web App, stylized with Tailwind CSS and hosted in AWS.",
   url: "https://enzomorales.net",
   src: "/src/assets/rpPreview.png",
-  iconSrcArray: ["/src/assets/reactIconWhite.svg", "/src/assets/nodeIconWhite.svg", "/src/assets/tsIconWhite.svg", "/src/assets/twIconWhite.svg"],
-  iconAltArray: ["React icon", "Node icon", "TypeScript icon", "Tailwind icon"],
+  iconSrcArray: ["/src/assets/reactIconWhite.svg", "/src/assets/nodeIconWhite.svg", "/src/assets/tsIconWhite.svg", "/src/assets/twIconWhite.svg", "/src/assets/awsIconWhite.svg"],
+  iconAltArray: ["React icon", "Node icon", "TypeScript icon", "Tailwind icon", "AWS icon"],
 }
 
 function Home() {
