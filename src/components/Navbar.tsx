ab57de8b022e964251aef548774b99df9801aca9
efmorales <54375116+efmorales@users.noreply.github.com>
2024-05-02 17:06:56 -0400

    added hover functionality to Home logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,12 +15,14 @@ const Navbar: React.FC<NavbarProps> = ({ scrollToHero, scrollToProjects }) => {
   return (
     <div className="w-full h-16 bg-[#002F38] flex justify-between items-center">
       <div className="bg-[#00948D] w-32 h-full flex justify-center items-center">
-        <HomeIcon className="h-6 w-6 text-black" />
+        <a href="https://enzomorales.net">
+          <HomeIcon className="h-7 w-7 text-slate-300 hover:scale-125 hover:text-pink-400 transition" />
+        </a>
       </div>
       <div className="hidden sm:flex px-6 h-full gap-4">
         <button onClick={scrollToHero} className="text-white hover:bg-[#00948D] px-3 py-2 rounded-md transition-colors duration-300">About</button>
         <button onClick={scrollToProjects} className="text-white hover:bg-[#00948D] px-3 py-2 rounded-md transition-colors duration-300">Projects</button>
-      
+
       </div>
       <div className="sm:hidden px-4 flex-col hover:bg-[#00948D] rounded-md transition-colors duration-300">
         <button
@@ -31,7 +33,7 @@ const Navbar: React.FC<NavbarProps> = ({ scrollToHero, scrollToProjects }) => {
           <div className="absolute left-0 z-50 bg-[#00948D] w-full flex flex-col items-center py-2">
             <button onClick={scrollToHero} className="text-white hover:bg-emerald-500 px-28 py-2 rounded-md transition-colors duration-300">About</button>
             <button onClick={scrollToProjects} className="text-white hover:bg-emerald-500 px-28 py-2 rounded-md transition-colors duration-300">Projects</button>
-           
+
           </div>
         )}
       </div>
